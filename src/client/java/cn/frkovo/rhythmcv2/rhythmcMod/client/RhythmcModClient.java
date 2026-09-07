d3c030d52db146582148cc6160eddcd9053fd959
Frkovo <1605532208@qq.com>
2026-09-08 00:37:08 +0800

    init: initial import of rhythmc-mod (Fabric client chart editor)

---
+++ src/client/java/cn/frkovo/rhythmcv2/rhythmcMod/client/RhythmcModClient.java
@@ -0,0 +1,10 @@
+package cn.frkovo.rhythmcv2.rhythmcMod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RhythmcModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/client/java/cn/frkovo/rhythmcv2/rhythmcMod/client/RhythmcModDataGenerator.java
@@ -0,0 +1,12 @@
+package cn.frkovo.rhythmcv2.rhythmcMod.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RhythmcModDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
